20494c2cc791e93538ae0e833f346a705661306e
andrea gauvreau <andrea.gauvreau31@gmail.com>
2022-11-04 22:44:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Switch from 'react-js-switch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'
+
+
+const themes = {
+  dark : {backgroundColor:'#ffffff20',background : 'black', color: 'white', transition: '0.5s'}, 
+  light: {backgroundColor:'#00000020',background:'white', color: 'black', transition: '0.5s'}}
+const ThemeContext = React.createContext(themes.light)
+
+
+function Header(){
+  const theme = React.useContext(ThemeContext)
+  return(
+    <div style={{background: theme.background, transition: theme.transition}}>
+    <h2 style={{color: theme.color, transition: theme.transition}}>Dark/light mode</h2>
+  </div>
+  )}
+
+function Main({darkMode, setDarkMode}){
+  const [switchText, setSwitchText]=useState('dark')
+
+  const handleChange=(e)=>{
+    setDarkMode(!darkMode)
+    setSwitchText(darkMode ? 'light':'dark')
+  }
+
+
+
+    const theme = React.useContext(ThemeContext)
+    return(
+      <div style={{background: theme.background, transition: theme.transition}}>
+      <SearchBar/> <br/> 
+      <label style={{color: theme.color, transition: theme.transition}}>switch to {switchText} mode
+      </label>
+      <Switch value={darkMode} onChange={handleChange} />
     </div>
-  );
-}
+    )}
 
-export default App;
+function Footer(){
+      const theme = React.useContext(ThemeContext)
+      return(
+        <div style={{background: theme.background, transition: theme.transition}}>
+        <h2 style={{color: theme.color, transition: theme.transition}}>Try it !</h2>
+      </div>
+      )}
+  
+
+      function SearchBar(){
+        const theme = React.useContext(ThemeContext)
+        return (<>
+          <input type="text" placeholder='...' style={{background:theme.backgroundColor}}/>
+          </>
+        )
+      }
+  
+  
+  function App(){  
+    const [darkMode, setDarkMode]= React.useState(true)  
+    const theme = darkMode ? themes.light : themes.dark
+
+    return(<div style={{background: theme.background, transition: theme.transition}} id='App'>
+  <ThemeContext.Provider value={theme}>
+    <Header/>
+    <Main darkMode={darkMode} setDarkMode={setDarkMode}/>
+    <Footer/>
+  </ThemeContext.Provider>
+    </div>
+    )}    
+  
+  export default App;
+
+
+
+
